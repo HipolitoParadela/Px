b6e4b2bceef58d0c7ca07a73bef99cf26d4e7d6c
Hipolito Paradela <31195000+HipolitoParadela@users.noreply.github.com>
2020-09-10 23:19:30 -0300

    Control

--- resto/V1/application/helpers/funciones_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    function plan_contratado(){
-        $plan = 3; /// plan inicial, 2 plan medio, 3 plan completo
-        return $plan;
-    }
-
-    
--- resto/V1/application/views/pruebas.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo $Datos;
-
-?>
-
-ñsdflkajsfd
